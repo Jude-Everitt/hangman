6168b14e98d1751bf9ba01c741f9d535e10d7e47
Jude-Everitt <everittjude@gmail.com>
2023-11-06 11:02:56 +0000

    milestone_3

--- milestone_3.py
+++ milestone_3.py
@@ -8,32 +8,24 @@ class Word:
        self.ask_for_input()
 
    def ask_for_input(self):
-       
-       guess = input("Enter a letter.\n")
-       print(guess)
+    
        while True:
-       
+           guess = input("Enter a letter.\n")
            if ((guess.isalpha() == True) and (len(guess) == 1)):
               self.check_guess(guess) 
               print("That is a valid input.")
-              return guess
-                              
 
            else:
               print("That is not a valid input. Try again.")
-              continue
-
+              
    def check_guess(self, guess):
-          
-       while True: 
            
            if (guess in self.word):
               print("Good guess!", guess, "is in the word!")
-              return guess
-              break
+              
            else:
               print("Sorry,", guess, "is not in the word. Try again.")
-           continue
+              
 
 Word(word_list)
 
