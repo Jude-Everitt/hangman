9f0cdc814037a544aedd96cb19fc6291436204a9
Jude-Everitt <everittjude@gmail.com>
2023-11-07 18:03:00 +0000

    milestone_4.py

---
+++ hangman/hangman.py
@@ -0,0 +1,38 @@
+import random
+##parameters
+word_list = ["pineapple", "coconut", "strawberry", "lime", "lemmon", "apple", "banana", "grape", "passionfruit", "melon", "watermelon", "orange"]
+num_lives = 5
+
+class Hangman(word_list, num_lives):
+   def __init__(self):
+##attributes
+      self.word = random.choice(self.word_list)
+      self.word_guessed = ["_" for char in self.word]
+      self.num_lives = num_lives
+      self.word_list = word_list
+      self.list_of_guesses = []
+
+   def check_guess(self, guess):
+      self.guess.lower()
+      if self.guess in self.word:
+         print("Good guess!", self.guess, ", is in the word!")
+         for char in self.word:
+            if self.guess == self.word[char]:
+               self.word_guessed[char] == self.guess
+      else:
+         self.num_lives -= 1
+         print("Unlucky!", self.guess, ", is not in the word")
+         print("You have ",self.num_lives," lives left!")
+
+   def ask_for_input(self):
+      while(True):
+         self.guess = input("Please enter a letter.\n")
+         if ((self.guess.isalpha() == False) or (len(self.guess) >= 2)):
+            print("Invalid input. Please, enter a single alphabetical character")
+         elif self.guess in self.list_of_guesses:
+            print("You have already tried that letter!")
+         else:
+            self.check_guess(self.guess)
+            self.list_of_guesses.append(self.guess)
+
+Hangman(word_list, num_lives == 5)
--- milestone_3.py
+++ milestone_3.py
@@ -7,25 +7,23 @@ class Word:
        self.word = random.choice(words)
        self.ask_for_input()
 
+
    def ask_for_input(self):
-    
        while True:
            guess = input("Enter a letter.\n")
            if ((guess.isalpha() == True) and (len(guess) == 1)):
               self.check_guess(guess) 
               print("That is a valid input.")
-
            else:
               print("That is not a valid input. Try again.")
-              
+
+
    def check_guess(self, guess):
-           
            if (guess in self.word):
               print("Good guess!", guess, "is in the word!")
-              
+              break
            else:
-              print("Sorry,", guess, "is not in the word. Try again.")
-              
+              print("Sorry,", guess, "is not in the word. Try again.")           
 
 Word(word_list)
 
