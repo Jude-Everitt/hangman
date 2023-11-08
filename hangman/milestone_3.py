b5a34ff844174c41081de94e95e986fece070525
Jude-Everitt <147021206+Jude-Everitt@users.noreply.github.com>
2023-11-08 21:22:23 +0000

    Add files via upload

---
+++ hangman/milestone_3.py
@@ -0,0 +1,33 @@
+# The code is importing the `random` module, which allows us to generate random numbers or make random
+# choices.
+import random
+
+word_list = ["mango", "lime", "pineapple", "coconut", "banana"]
+word = random.choice(word_list)
+guess = input("Enter a letter.\n")
+
+def check_guess():
+   """
+   The function checks if a guess is in a word and prints a corresponding message.
+   """
+   if (guess in word):
+      print("Good guess!", guess, "is in the word!")      
+   else:
+      print("Sorry,", guess, "is not in the word. Try again.") 
+
+def ask_for_input():
+   """
+   The function asks the user to enter a single letter and checks if the input is valid.
+   """
+   while (True):
+      guess = input("Enter a letter.\n")
+      if ((guess.isalpha() == True) and (len(guess) == 1)):
+         print("That is a valid input.")
+         break
+      else:
+         print("That is not a valid input. Try again.")
+   check_guess() 
+   
+ask_for_input()
+
+
